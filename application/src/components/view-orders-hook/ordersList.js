bedc09fa401cc6005eaf0a518bf3cc6b0494aa2a
angelz510 <angelzrod4@gmail.com>
2021-10-27 15:48:23 -0700

    refactor(orders): removed commented out code

--- application/src/components/view-orders-hook/ordersList.js
+++ application/src/components/view-orders-hook/ordersList.js
@@ -118,33 +118,4 @@ const OrdersList = (props) => {
   );
 };
 
-// return orders.map((order) => {
-//   const createdDate = new Date(order.createdAt);
-//   return (
-//     <div className="row view-order-container" key={order._id}>
-//       <div className="col-md-4 view-order-left-col p-3">
-//         <h2>{order.order_item}</h2>
-//         <p>Ordered by: {order.ordered_by || ""}</p>
-//       </div>
-//       <div className="col-md-4 d-flex view-order-middle-col">
-//         <p>
-//           Order placed at{" "}
-//           {`${createdDate.getHours()}:${
-//             (createdDate.getMinutes() < 10 ? "0" : "") + createdDate.getMinutes()
-//           }:${(createdDate.getSeconds() < 10 ? "0" : "") + createdDate.getSeconds()}`}
-//         </p>
-//         <p>Quantity: {order.quantity}</p>
-//       </div>
-//       <div className="col-md-4 view-order-right-col">
-//         <button className="btn btn-success" onClick={() => editOrder(order._id)}>
-//           Edit
-//         </button>
-//         <button className="btn btn-danger" onClick={() => deleteOrder(order._id)}>
-//           Delete
-//         </button>
-//       </div>
-//     </div>
-//   );
-// });
-
 export default OrdersList;
